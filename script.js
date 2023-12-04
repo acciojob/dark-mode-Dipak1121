e624d88104769f2c885b147b91b8e9d909ee9431
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-12-04 15:25:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+function toggleDarkMode() {
+        const container = document.getElementById('container');
+        container.classList.toggle('dark-mode');
+
+        // Check if dark mode is enabled and store the preference in Local Storage
+        const isDarkModeEnabled = container.classList.contains('dark-mode');
+        localStorage.setItem('darkMode', isDarkModeEnabled);
+    }
+
+    // Check for dark mode preference in Local Storage on page load
+    document.addEventListener('DOMContentLoaded', () => {
+        const container = document.getElementById('container');
+        const isDarkModeEnabled = localStorage.getItem('darkMode') === 'true';
+
+        if (isDarkModeEnabled) {
+            container.classList.add('dark-mode');
+        }
+    });
